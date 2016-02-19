f330708d4475c7a9d1184987081caedf5085cb40
sudhakarphule1 <sudhakar.phule@twinprimelabs.com>
2016-02-19 16:44:54 +0530

    First skeleton of order app

---
+++ ui/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+import ngMaterial from 'angular-material';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
