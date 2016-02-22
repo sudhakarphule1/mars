e8ca89a3709e1f191758c3491b1b3708fb384846
sudhakarphule1 <sudhakar.phule@twinprimelabs.com>
2016-02-22 17:08:21 +0530

    Added ng2-material list-view component

--- ui/app/app.component.ts
+++ ui/app/app.component.ts
@@ -1,8 +1,56 @@
-import {Component} from 'angular2/core';
-import ngMaterial from 'angular-material';
+import {View, Component} from 'angular2/core';
+import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from "ng2-material/all";
+
+let imagePath = 'public/images/avatars/avatar11.svg';
+
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    selector: 'my-app'
 })
-export class AppComponent { }
+@View({
+  templateUrl: 'app/app.component.html'
+})
+export class AppComponent {
+phones = [
+    {type: 'Home', number: '(555) 251-1234'},
+    {type: 'Cell', number: '(555) 786-9841'},
+    {type: 'Office', number: '(555) 314-1592'}
+  ];
+  todos = [
+    {
+      face: imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },
+    {
+      face: imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },
+    {
+      face: imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },
+    {
+      face: imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },
+    {
+      face: imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },
+  ];
+}
