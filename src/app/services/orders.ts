46778f46f71327f5bf706fe2687faf3be516c831
chetan unkule <chetan.unkule@twinprimelabs.com>
2016-03-08 15:59:40 +0530

    Commit after adding all products and orders in JSON file

---
+++ src/app/services/orders.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by chetan on 8/3/16.
+ */
+import {Injectable} from 'angular2/core';
+import {Http, URLSearchParams} from 'angular2/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AllOrders {
+  constructor(private http: Http){}
+
+  getOrders(){
+    return this.makeRequest();
+  }
+
+  private makeRequest(){
+/*    let params = new URLSearchParams();
+    params.set('per_page', '100');*/
+    let url = `app/components/myOrders/myOrders.json`;
+    console.log(url);
+    return this.http.get(url).map((res) => res.json());
+  }
+}
