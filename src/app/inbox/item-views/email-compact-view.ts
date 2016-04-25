b0a4f7e06c027a793fabcdfab7f7670a667647a2
Waqar Shaikh <waqar.shaikh@twinprimelabs.com>
2016-04-25 14:58:23 +0530

    html rendiring in inbox .

--- src/app/inbox/item-views/email-compact-view.ts
+++ src/app/inbox/item-views/email-compact-view.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
 import {MATERIAL_DIRECTIVES} from "ng2-material/all";
 import {TimeAgoPipe} from "angular2-moment/TimeAgoPipe";
+import {LeftNavigation} from "./../../share/components/left.navigation.component";
 
 @Component({
   selector: 'ib-email-compact-view',
   templateUrl: 'app/inbox/item-views/email-compact-view.html',
   styleUrls: ['app/inbox/item-views/email-compact-view.css'],
-  directives: [MATERIAL_DIRECTIVES],
+  directives: [MATERIAL_DIRECTIVES, LeftNavigation],
   pipes: [TimeAgoPipe],
   inputs: ['email','isDisabled']
 })
