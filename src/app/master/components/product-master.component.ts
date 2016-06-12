d4588fb3ddb416f7f66ded5c3ab545199cf00da7
sudhakar.phule@techprimelab.com <sudhakar.phule@techprimelab.com>
2016-06-13 01:34:56 +0530

    inputfieldEMPTYvalidation

--- src/app/master/components/product-master.component.ts
+++ src/app/master/components/product-master.component.ts
@@ -1,52 +1,91 @@
 import {Component} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
-import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from "ng2-material";
+//import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {Item} from "../../model/item";
+import {ProductService} from "../services/productmaster.service";
+import {ProductsFilterPipe} from "../../order/components/products-filter.pipe";
 
 @Component({
   selector: 'ib-product-master',
   templateUrl: 'app/master/components/product-master.component.html',
-  styleUrls: [''],
-  directives: [ROUTER_DIRECTIVES]
+  providers: [ProductService],
+  styles:[`
+  .empty{
+  border: 3px solid #F00;
+  }
+
+input:focus {
+  height: 50px;
+  width: 11em;
+  word-wrap: break-word;
+}
+`],
+  pipes:[ProductsFilterPipe]
 })
 
-
 export class ProductMaster {
-/*
-  orders = [{companyName: 'Infosys',
-    orderDetails: '5 Tissue boxes, 10 Hand dryers, 20 paper towels',
-    orderDate: '03/24/2016',
-    orderType: 'email'},
-    {companyName: 'HSBC',
-      orderDetails: '5 Tissue boxes, 10 Hand dryers, 20 paper towels',
-      orderDate: '02/10/2016',
-      orderType: 'online'},
-    {companyName: 'IBM',
-      orderDetails: '5 Tissue boxes, 10 Hand dryers, 20 paper towels',
-      orderDate: '06/30/2016',
-      orderType: 'phone'},
-    {companyName: 'Cognizant',
-      orderDetails: '5 Tissue boxes, 10 Hand dryers, 20 paper towels',
-      orderDate: '02/01/2016',
-      orderType: 'manual'},
-    {companyName: 'Quick Heal',
-      orderDetails: '5 Tissue boxes, 10 Hand dryers, 20 paper towels',
-      orderDate: '02/20/2016',
-      orderType: 'email'},
-    {companyName: 'Infosys',
-      orderDetails: '5 Tissue boxes, 10 Hand dryers, 20 paper towels',
-      orderDate: '03/24/2016',
-      orderType: 'email'},
-    {companyName: 'HSBC',
-      orderDetails: '5 Tissue boxes, 10 Hand dryers, 20 paper towels',
-      orderDate: '02/10/2016',
-      orderType: 'online'},
-    {companyName: 'IBM',
-      orderDetails: '5 Tissue boxes, 10 Hand dryers, 20 paper towels',
-      orderDate: '06/30/2016',
-      orderType: 'phone'}];
+  //public buttonMode = true;
+  entityname:string = '';debugger
+  isempty = false;
+  myid:any;
+  items:Array<Item>;
+  isVisibleDeleteButton: Array<Boolean> =new Array<Boolean>();
+  constructor(private products:ProductService) {
 
-  selectedItem: string;
+    products.getAllProducts()
+      .subscribe(res => {
+        this.items = res;
+      });
+  }
 
-  displayDetails(item) { this.selectedItem = item; }*/
+  setDeleteButtonValue(itemindex) {
+    //this.buttonMode = !this.buttonMode;
+    this.isVisibleDeleteButton[itemindex] = false;
+    for(var index : number = 0; index < this.items.length;index++){
+      if(index != itemindex){
+        this.isVisibleDeleteButton[index] = true;
+      }
+    }
+  }
+
+  handleKeyEvent(event) {
+    console.log(event.keyCode);
+
+    if (event.keyCode == 37) {
+
+    }
+
+    if (event.keyCode == 38) {
+
+    }
+
+    if (event.keyCode == 39) {
+
+    }
+
+    if (event.keyCode == 40) {
+
+    }
+  }
+
+  deleteRow(deleteItem) {
+    this.products.deleteProduct(deleteItem).subscribe();
+    this.products.getAllProducts()
+      .subscribe(res => {
+        this.items = res;
+      });
+  }
+
+  onRowClick(updateItem,elementName) {
+    if(elementName.length != 0) {
+      this.products.updateProduct(updateItem).subscribe(res => {
+        console.log("res" + res);
+        this.items = res;
+      });
+    }
+    else{
+      this.myid=updateItem._id;
+      this.isempty=true;
+    }
+  }
+
 }
