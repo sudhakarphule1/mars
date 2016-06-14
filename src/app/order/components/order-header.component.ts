7ccc7400f83fa93775d5d95c7c7fe73fcc57c5db
sudhakar.phule@techprimelab.com <sudhakar.phule@techprimelab.com>
2016-06-14 15:45:00 +0530

    changed date format

--- src/app/order/components/order-header.component.ts
+++ src/app/order/components/order-header.component.ts
@@ -86,13 +86,12 @@ export class OrderHeader implements OnInit {
         }
         this.orderObservableService.changeOrderObject(this.currentOrder);
         this.selectedCustomer = this.currentOrder.customer;
-        this.orderDate = this.currentOrder.orderDate;
-        this.completionDate = this.currentOrder.completionDate;
-        this.orderPlacedDate =this.currentOrder.orderPlacedDate;
-        this.transitDate = this.currentOrder.transitDate;
+        this.orderDate = this.currentOrder.orderDate.toDateString();
+        this.completionDate = this.currentOrder.completionDate.toDateString();
+        this.orderPlacedDate =this.currentOrder.orderPlacedDate.toDateString();
+        this.transitDate = this.currentOrder.transitDate.toDateString();
       });
 
-      this.commonobservable.changeEditFlag(true);
 
     }
 
